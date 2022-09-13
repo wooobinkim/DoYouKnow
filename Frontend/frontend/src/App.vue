12c6835873b596c73afa8b4894336dfd6d823248
Gungil Bae <97593802+geon-gil@users.noreply.github.com>
2022-09-13 13:51:48 +0900

    feat: Add skeleton code

---
+++ Frontend/frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <MainPage />
+</template>
+
+<script>
+import MainPage from "@/views/MainPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainPage,
+  },
+};
+</script>
+
+<style></style>
---
+++ Frontend/frontend/src/components/Main/MainGlobe.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>mainglobe</h2>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
