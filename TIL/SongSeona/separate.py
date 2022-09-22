6b6d9d5da6090f63de9a308627d7b2e5e49dd786
rladnqls98 <rladnqls98@gmail.com>
2022-09-22 10:44:17 +0900

    data process code update

--- TIL/SongSeona/separate.py
+++ TIL/SongSeona/separate.py
@@ -5,18 +5,18 @@ import os
 from datetime import datetime
 
 connection = pymysql.connect(
-    user='b208', 
-    passwd='b208root', 
-    host='43.201.37.208', 
-    db='DoYouKnow', 
+    user='b208',
+    passwd='b208root',
+    host='43.201.37.208',
+    db='DoYouKnow',
     charset='utf8',
     cursorclass=pymysql.cursors.DictCursor
 )
 with connection.cursor() as cursor:
-    .
     try:
         sql = "SELECT * FROM keywordtmp"
         insertsql = "insert into keyword (count,date,name,category_id,nation_id) values (%s,%s,%s,%s,%s)"
+        trunsql = "TRUNCATE keywordtmp"
 
         cursor.execute(sql)
         results = cursor.fetchall()
@@ -32,7 +32,9 @@ with connection.cursor() as cursor:
             val=(count,datetime.strptime(datas[1],'%Y-%m-%d'),datas[0],datas[3],datas[2])
             cursor.execute(insertsql,val)
             connection.commit()
-                
+            
+        cursor.execute(trunsql)
+        connection.commit()
     except:
         print ("Error: unable to fecth data")
 connection.close()
