dff4f00c6fab18d1e2188ee318e39a217a3ac121
hohyun <hohyun323@naver.com>
2022-09-27 21:27:11 +0900

    test: 💍 test

--- Frontend/src/api/BackendAPI.js
+++ Frontend/src/api/BackendAPI.js
@@ -1,6 +1,4 @@
-// const BE = 'http://127.0.0.1:8080'
-// const BE = 'https://j7b208.p.ssafy.io/api/'
-const BE = 'http://j7b208.p.ssafy.io:8080/api/'
+const BE = 'https://j7b208.p.ssafy.io/api/'
 
 
 // DYKC API
