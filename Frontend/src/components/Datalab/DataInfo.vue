60d90f4689365b17c87a487ef7c37eb602f1ce4e
hohyun <hohyun323@naver.com>
2022-10-04 10:26:06 +0900

    style: 💄 datalab count

--- Frontend/src/components/Datalab/DataInfo.vue
+++ Frontend/src/components/Datalab/DataInfo.vue
@@ -1,23 +1,19 @@
 <template>
-  <div class="total-container">
-      <div class="nation-total">
-        <template v-if="this.getNationRate">
-          총 데이터량
-          {{ this.getNationRate.nationCount.toLocaleString("ko-KR") }}개
-        </template>
-      </div>
-      <div class="nation-percentage">
-        <template v-if="this.getNationRate">
-          전체의 {{ this.getNationRate.nationRate }}%
-        </template>
-      </div>
-
+  <div class="total-container" v-if="this.getNationRate">
+    <div class="sub1-box">
+      <div class="first_box">총 데이터</div>
+      <div class="second_box">전체 데이터의 {{ this.getNationRate.nationRate }}%</div>
     </div>
+    <div class="sub2-box">
+      <span class="half-highlight">{{ this.getNationRate.nationCount.toLocaleString("ko-KR") }}개</span>
+    </div>
+  </div>
 
 </template>
 
 <script>
 import { mapGetters, useStore } from "vuex";
+
 export default {
   setup() {
     const store = useStore();
@@ -39,57 +35,49 @@ export default {
 
 
 <style>
-.total-container {
-  display: flex;
-  margin-top: 3.8rem;
-  margin-right: 2rem;
-  width: 184px;
-  height: 65px;
-}
-.nation-total {
-  background: #f5f5f5;
-  width: 20rem;
-  height: 4rem;
-  border-radius: 35px;
-  /* box-shadow: 3px 3px 4px gray; */
-  text-align: center;
-  line-height: 4rem;
-  font-size: 1.5rem;
-  font-weight: bold;
-  margin-right: 1.5rem;
-}
-.nation-percentage {
-  background: #f5f5f5;
-  width: 20rem;
-  height: 4rem;
-  border-radius: 35px;
-  /* box-shadow: 3px 3px 4px gray; */
-  text-align: center;
-  line-height: 4rem;
-  font-size: 1.5rem;
-  font-weight: bold;
-}
-.data-container {
-  display: flex;
-}
-
-/* .data-container {
-  display: flex;
-  margin-left: 1.5rem;
-}
-.total {
-  width: 250px;
-  height: 30px;
-  background: white;
-  opacity: 71%;
-  border-radius: 30px;
-}
-.percentage {
-  margin-left: 1rem;
-  width: 100px;
-  height: 30px;
-  background: white;
-  opacity: 71%;
-  border-radius: 30px;
-} */
+  @font-face {
+    font-family: 'SF_IceLemon';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2106@1.1/SF_IceLemon.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+      font-family: 'HS-Regular';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/HS-Regular.woff') format('woff');
+      font-weight: normal;
+      font-style: normal;
+  }
+  @font-face {
+    font-family: 'establishRoomNo703OTF';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2112@1.0/establishRoomNo703OTF.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  .total-container{
+    display: flex;
+    flex-direction: column;
+  }
+  .sub1-box{
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-end;
+    margin-bottom: 1rem;
+  }
+  .sub2-box{
+    font-size: 2.5rem;
+    font-family: 'SF_IceLemon';
+  }
+  .half-highlight {
+    background: linear-gradient(180deg,rgba(255,255,255,0) 80%, #edf23e 50%);
+  }
+  .first_box{
+    font-size: 2rem;
+    font-family: 'HS-Regular';
+  }
+  .second_box{
+    padding: 0.5rem;
+    background-color: white;
+    border-radius: 15px;
+    font-family: 'establishRoomNo703OTF';
+  }
 </style>
