bd9d4ba3dea53a8503870c30a275a3fbd52117d1
DESKTOP-KVCQHCD\SSAFY <niinp@naver.com>
2022-10-04 17:35:12 +0900

    style: 💄 keyword rank 아이템 css 스타일링

--- Frontend/src/components/Datalab/KeywordRank.vue
+++ Frontend/src/components/Datalab/KeywordRank.vue
@@ -10,19 +10,25 @@
         </button>
       </template>
       <br />
-      <div style="margin-top:2rem;">
+      <div style="margin-top:1rem">
         <div style="display:flex; flex-direction: column; float: right;">
           <div v-for="period in this.getPeriod" :key="period.value">
             <button @click="setPeriod(period.value)" :class="{'btn-common': true, 'btn-period-active' : PeriodNo === period.value}">{{ period.text }}</button>
           </div>
         </div>
 
-        <!-- <section id="content1"> -->
-        <template v-for="(keyword, index) in this.getKeywordRank" :key="keyword">
-          <div class="rankkeyword" v-if="index < 5">
-            <div @click="setOneKeyword(keyword.name)">{{index+1}}. {{ keyword.name }}</div>
-          </div>
-        </template>
+        <div class="leaderboard">
+          <ol>
+            <template v-for="(keyword, index) in this.getKeywordRank" :key="keyword">
+              <li data-aos="fade-right" v-if="index<5">
+                <mark class="rankkeyword" @click="setOneKeyword(keyword.name)">
+                  {{ keyword.name }}
+                </mark>
+              </li>
+            </template>
+          </ol>
+        </div>
+        
       </div>
       <!-- </section> -->
     </div>
@@ -143,7 +149,16 @@ export default {
   text-align: left;
   margin-left: 1rem;
   font-weight: bold;
+  position: relative;
+  z-index: 1;
+  font-size: 20px;
+  background: #f5f5f5;
+  /* padding: 18px 10px 18px 50px; */
+  cursor: pointer;
+  backface-visibility: hidden;
+  transform: translateZ(0) scale(1.0, 1.0);
 }
+
 p {
   margin: 0;
 }
@@ -210,7 +225,7 @@ input:checked + label {
   text-decoration: none;
   font-weight: 600;
   transition: 0.25s;
-  margin: 5px 2px;
+  margin: 0px 2px;
   background-color: aliceblue;
   color: #1e6b7b;
 }
@@ -235,5 +250,180 @@ input:checked + label {
 .btn-period-active {
   background-color: #77af9c;
   color: #d7fff1;
+}
+ /* 랭킹 아이템 */
+.leaderboard {
+    position: absolute;
+    top: 40%;
+    left: 45%;
+    transform: translate(-50%, -50%);
+    width: 350px;
+    height: auto;
+    background: transparent;
+    border-radius: 10px;
+}
+
+.leaderboard h1 {
+    font-size: 18px;
+    color: #e1e1e1;
+    padding: 12px 13px 18px;
+}
+
+.leaderboard ol {
+    counter-reset: leaderboard;
+    list-style: none;
+    padding: 0;
+}
+
+.leaderboard ol li {
+    position: relative;
+    z-index: 1;
+    font-size: 14px;
+    counter-increment: leaderboard;
+    padding: 18px 10px 18px 50px;
+    cursor: pointer;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    transform: translateZ(0) scale(1, 1);
+    border-radius: 20px;
+}
+
+.leaderboard ol li::before {
+    content: counter(leaderboard);
+    position: absolute;
+    z-index: 2;
+    top: 10px;
+    left: 15px;
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+    color: #c24448;
+    background: #fff;
+    border-radius: 20px;
+    text-align: center;
+}
+
+.leaderboard ol li mark {
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    width: 85%;
+    height: 100%;
+    padding: 5px 10px 0px 50px;
+    margin: 0;
+    background: none;
+    color: #fff;
+}
+
+.leaderboard ol li small {
+    position: relative;
+    z-index: 2;
+    display: block;
+    text-align: right;
+}
+
+.leaderboard ol li::after {
+    content: "";
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #fa6855;
+    box-shadow: 0 3px 0 rgba(0, 0, 0, 0.08);
+    transition: all 0.3s ease-in-out;
+    opacity: 0;
+    border-radius: 20px;
+}
+
+.leaderboard ol li:nth-child(1) {
+    background: #fa6855;
+}
+
+.leaderboard ol li:nth-child(1)::after {
+    background: #fa6855;
+}
+
+.leaderboard ol li:nth-child(2) {
+    background: #e0574f;
+}
+
+.leaderboard ol li:nth-child(2)::after {
+    background: #e0574f;
+    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.08);
+}
+
+.leaderboard ol li:nth-child(2) mark::before,
+.leaderboard ol li:nth-child(2) mark::after {
+    border-top: 6px solid #ba4741;
+    bottom: -7px;
+}
+
+.leaderboard ol li:nth-child(3) {
+    background: #d7514d;
+}
+
+.leaderboard ol li:nth-child(3)::after {
+    background: #d7514d;
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.11);
+}
+
+.leaderboard ol li:nth-child(3) mark::before,
+.leaderboard ol li:nth-child(3) mark::after {
+    border-top: 2px solid #b0433f;
+    bottom: -3px;
+}
+
+.leaderboard ol li:nth-child(4) {
+    background: #cd4b4b;
+}
+
+.leaderboard ol li:nth-child(4)::after {
+    background: #cd4b4b;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.15);
+}
+
+.leaderboard ol li:nth-child(4) mark::before,
+.leaderboard ol li:nth-child(4) mark::after {
+    top: -7px;
+    bottom: auto;
+    border-top: none;
+    border-bottom: 6px solid #a63d3d;
+}
+
+.leaderboard ol li:nth-child(5) {
+    background: #c24448;
+}
+
+.leaderboard ol li:nth-child(5)::after {
+    background: #c24448;
+    box-shadow: 0 -2.5px 0 rgba(0, 0, 0, 0.12);
+    border-radius: 20px;
+}
+
+.leaderboard ol li:nth-child(5) mark::before,
+.leaderboard ol li:nth-child(5) mark::after {
+    top: -9px;
+    bottom: auto;
+    border-top: none;
+    border-bottom: 8px solid #993639;
+}
+
+.leaderboard ol li:hover {
+    z-index: 2;
+    overflow: visible;
+}
+
+.leaderboard ol li:hover::after {
+    opacity: 1;
+    transform: scaleX(1.06) scaleY(1.03);
+}
+
+.leaderboard ol li:hover mark::before,
+.leaderboard ol li:hover mark::after {
+    opacity: 1;
+    transition: all 0.35s ease-in-out;
 }
 </style>
